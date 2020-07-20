0d1041a9b318021e27055cb010ca2b436cbe0410
Radyslav Dmytrenko <1991barbossa@gmail.com>
2020-07-20 10:59:17 +0300

    accordion is complete(js, jQuery, css) at section Questions

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,11 @@ $(function(){
     infinite: false,
   });
 
+//Accordion
+
+  $('.questions__item-title').on('click', function() {
+    $('.questions__item').removeClass('questions__item--active');
+    $(this).parent().addClass('questions__item--active');
+  });
+
 });
